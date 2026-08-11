70fa8ae22934e4dab2bcfe1eff710fb49fad2919
Md Arsh Azam <arshazam7646@gmail.com>
2026-08-11 13:59:07 +0530

    Add login and logout functionality

--- bookstore_site/settings.py
+++ bookstore_site/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', "django-insecure-ou@b#d^j_7&^5aui--1ek$td#+q3$s4otc0t(-uoeq*4(bz7e%")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
